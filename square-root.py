392815e9c5f2b1db29187e69fe27acdca0878e38
Amr Guaily <amrguaily@gmail.com>
2023-11-28 21:14:50 +0200

    fix square-root to handle numbers less than 1

--- square-root.py
+++ square-root.py
@@ -4,11 +4,16 @@
 # answer: 1.4142135623730951. so we need to thik about the concept of approximation
 # We will use binary search technique
 
+# Question: Does this implementation work for all positive values?
+# Answer: No, it doesn't - try getting the square root of 0.5 => the square root of 0.5 is not lie between [0, 0.5] so the loop will run forever (doesn't terminate)
+# To fix: high = max(1.0, x)
+
 x = float(input("Enter a number: "))
 epsilon = 0.01
 numGuesses = 0
 low = 0.0
-high = x
+# high = x 
+high = max(1.0, x) # we need to make sure that high is greater than 1
 ans = (high + low) / 2.0
 
 while abs(ans ** 2 - x) >= epsilon and ans <= x:
