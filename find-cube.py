ea6fa32b9f80d0c71f7498fd83f4c0fdb2d0698e
Amr Guaily <amrguaily@gmail.com>
2023-11-28 18:13:12 +0200

    Find the cube root of a perfect cube - for-loop version

--- find-cube.py
+++ find-cube.py
@@ -1,12 +1,31 @@
 # Find the cube root of a perfect cube
+
+# Question: what's kind of algorithm this is !!?
+# Answer: This an example of Guess&Check and it's called "exhaustive enumeration"
+# Exhaustive enumeration is a silly technique (takes a lot of gusses). we refer to such algorithms as brute force algorithm that searches all possible
+
 x = int (input("Enter a integer: "))
-ans = 0
 
-while ans * ans * ans  < abs(x):
-    ans = ans + 1
+# While loop version
+# ans = 0
+# while ans * ans * ans  < abs(x):
+#     ans = ans + 1
+#     print ("current guess = " + str(ans))
+
+# if ans * ans * ans != abs(x):
+#     print (str(x) + " Is not a perfect cube")
+# else:
+#     if x < 0:
+#         ans = -ans
+#     print ("Cube root of " + str(x) + " is " + str(ans))
+
+# For loop version
+for ans in range(abs( x + 1)):
     print ("current guess = " + str(ans))
+    if ans ** 3 == abs(x):
+        break
 
-if ans * ans * ans != abs(x):
+if ans ** 3 != abs(x):
     print (str(x) + " Is not a perfect cube")
 else:
     if x < 0:
